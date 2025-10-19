49750b301c4ae48c089b43de02c20ea03f7d3e73
amurovnaa <amurovna09@gmail.com>
2025-10-19 18:07:14 +0300

    fix quotes and use zenquotes

--- src/utils/getRandomQuote.js
+++ src/utils/getRandomQuote.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-const fallbackQuotes = ['Keep going!', 'You can do it!', 'Never give up!'];
 
 export const getRandomQuote = async () => {
   try {
-    const res = await axios.get('https://api.quotable.io/random');
-    return res.data.content;
-  } catch {
-    console.warn('Quotable fetch failed, using fallback.');
-    return fallbackQuotes[Math.floor(Math.random() * fallbackQuotes.length)];
+    const res = await axios.get('https://zenquotes.io/api/random');
+    const quote = res.data[0];
+    return `${quote.q}`;
+  } catch (err) {
+    console.error('Quote fetch failed:', err.message);
+    return 'Keep going!';
   }
 };
